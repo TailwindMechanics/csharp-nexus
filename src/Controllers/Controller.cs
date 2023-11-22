1dafae2d1340ae8d59d1d88d70c5960846780ba0
Tailwind Mechanics <shea@tailwindmechanics.com>
2023-11-22 20:24:26 +0000

    Standardised ship logging for endpoints

--- src/Controllers/Controller.cs
+++ src/Controllers/Controller.cs
@@ -9,6 +9,7 @@ using System.Text;
 using Serilog;
 
 using Neurocache.NodeRouter;
+using Neurocache.ShipsInfo;
 using Neurocache.Schema;
 
 namespace Neurocache.Controllers
@@ -19,23 +20,25 @@ namespace Neurocache.Controllers
         [HttpPost("kill")]
         public IActionResult Kill()
         {
-            Log.Information("Killing all sessions");
+            Ships.Log("Killing all sessions");
             BulletinRouter.KillSubject.OnNext(Unit.Default);
-            return Ok($"Killed");
+            return Ok();
         }
 
         [HttpPost("stop")]
         public IActionResult Stop([FromBody] StopSessionRequest body)
         {
+            Ships.Log("Stopping session");
             body.Deconstruct(out var sessionToken);
-            Log.Information($"Stopping session: {sessionToken}");
             BulletinRouter.StopSubject.OnNext(sessionToken);
-            return Ok($"Stopped session: {sessionToken}");
+            return Ok();
         }
 
         [HttpPost("run")]
         public async Task Run()
         {
+            Ships.Log($"Received operation request");
+
             using var reader = new StreamReader(Request.Body);
             var body = await reader.ReadToEndAsync();
             var bulletin = JsonConvert.DeserializeObject<Bulletin>(body)!;
@@ -43,7 +46,6 @@ namespace Neurocache.Controllers
             int completedNodeCount = 0;
             var channel = Channel.CreateUnbounded<string>();
 
-            Log.Information($"Received bulletin");
             BulletinRouter.RecordStream
                 .Where(rec => rec.SessionToken == bulletin.SessionToken)
                 .Subscribe(rec =>
@@ -53,10 +55,10 @@ namespace Neurocache.Controllers
                     var serialized = JsonConvert.SerializeObject(rec);
                     channel.Writer.TryWrite(serialized);
 
-                    Log.Information($"Emitting record:{rec.Payload}, from:{rec.NodeId}, is final:{rec.IsFinal}");
+                    Ships.Log($"Emitting report:{rec.Payload}, from:{rec.NodeId}, is final:{rec.IsFinal}");
                     if (completedNodeCount >= bulletin.NodeIds.Count)
                     {
-                        Log.Information("All nodes completed, closing channel");
+                        Ships.Log("All nodes completed, closing channel");
                         channel.Writer.TryComplete();
                     }
                 });
